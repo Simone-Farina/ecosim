e0d4ef86e5963ea7ef3568a2f4d12a666a45b2eb
Simone <farinasimone2411@gmail.com>
2025-12-06 19:20:01 +0100

    refactor

--- src/main/scala/domain/Firm.scala
+++ src/main/scala/domain/Firm.scala
@@ -1,6 +1,6 @@
 package domain
 
-import cats.data.ValidatedNel
+import cats.data.{Validated, ValidatedNel}
 import cats.syntax.all._
 import domain.Newtypes._
 
@@ -16,24 +16,22 @@ case class Firm(
   private def validateBudget(b: Money): ValidatedNel[String, Money] =
     if (b < Money(0)) "Negative budget".invalidNel else b.validNel
 
-  private def validateCash(firm: Firm, b: Money): ValidatedNel[String, Money] =
+  private def validateFunds(firm: Firm, b: Money): ValidatedNel[String, Money] =
     if (firm.cash < b) "Insufficient funds".invalidNel else b.validNel
 
-  private def validateInvestment(b: Money): ValidatedNel[String, Money] =
-    if (b < Money(1000)) "Insufficient resources for tech improvement".invalidNel else b.validNel
-
   def produce(budget: Money): Either[String, Firm] = {
-    (validateBudget(budget), validateCash(this, budget)).mapN {
+    (validateBudget(budget), validateFunds(this, budget)).mapN {
       case (budget, _) => this.copy(
-        cash = this.cash - budget,
+        cash = cash - budget,
         quantity = Quantity.unsafe(this.quantity.value + (budget.value * tech).toInt)
       )
     }
   }.toEither.leftMap(_.mkString_(" & "))
 
-  def investInRnD(budget: Money): Either[String, Firm] =
-    validateInvestment(budget)
-      .map(_ => this.copy(tech = tech + 0.1))
-      .toEither
-      .leftMap(_.mkString_(" & "))
+  def investInRnD(budget: Money): Either[String, Firm] = {
+    validateFunds(this, budget).map {
+      case validatedBudget@_ if validatedBudget < Money(1000) => this.copy(cash = cash - budget)
+      case _ => this.copy(cash = cash - budget, tech = tech + 0.1)
+    }
+  }.toEither.leftMap(_.mkString_(" & "))
 }
